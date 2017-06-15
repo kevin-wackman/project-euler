9042a57d02f63a32788de564ee7a031dcaea0cb6
kevin_wackman <kevin.wackman@gmail.com>
2017-06-15 17:46:17 -0500

    Reached correct solution for e243.hs

--- e243.hs
+++ e243.hs
@@ -1,42 +1,20 @@
---UNFINISHED--
-
-import Data.Ratio
-import Data.List
 import Data.Numbers.Primes
+import Data.List
+import Data.Ratio
 
+isCoprime :: Integral a => a -> a -> Bool
+isCoprime n = (== 1) . gcd n
 
-numFactors :: (Integral a) => a -> Int
-numFactors n = (product $ map ((+1) . length) (group (primeFactors n)))
+numCoprimes :: (Integral a) => a -> a
+numCoprimes n = round $ (*(fromIntegral n)) $ product $ map (\x -> (1-(1/(fromIntegral x))))$ nub $ primeFactors n
 
 resilience :: Integer -> Ratio Integer
-resilience n = let
-    ns = [(x%n) | x<-[1..(n-1)]]
-    numResilient = fromIntegral $ length $ filter ((== n) . denominator) ns
-    in numResilient%(n-1)
+resilience n = numCoprimes n % (n-1)
+
+main = do
+    print $ head $ filter (< (15499%94744)) $ map resilience $ uglyFun [100000000..1000000000]
     
-myMin :: (Ord b) => (a -> b) -> [a] -> a
-myMin fun (x:xs) = myMin' fun xs (x,(fun x))
-
-myMin' :: (Ord b) => (a -> b) -> [a] -> (a,b) -> a
-myMin' _ [] (a,_) = a
-myMin' fun (x:xs) (a,b)
-    | (fun x) < b = myMin' fun xs (x,(fun x))
-    | otherwise   = myMin' fun xs (a,b)
-    
-multPrimes :: [Integer]
-multPrimes = scanl1 (*) primes
-
-
-
 isFactor :: Integer -> Integer -> Bool
 isFactor n m = mod m n == 0
 
-
-    
-main = do
-    print $ head $ filter ((< (15499%94744)) . resilience) $ take 20 $ filter even $ filter (isFactor 3) $ filter (isFactor 5) $ filter (isFactor 7) $ filter (isFactor 11) $ filter (isFactor 13) $ [100000000..6469693230]
-
-
-    
-    
-
+uglyFun = filter (isFactor 23) . filter (isFactor 19) . filter (isFactor 17) . filter (isFactor 13) . filter (isFactor 11) . filter (isFactor 7) . filter (isFactor 5) . filter (isFactor 3) . filter even
