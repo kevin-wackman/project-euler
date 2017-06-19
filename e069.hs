c2fa6d32fe12086e287e4e9256bd36a48a21b522
kevin_wackman <kevin.wackman@gmail.com>
2017-06-19 14:08:51 -0500

    Changed e069 to not rely on a magic number and instead solve the problem without prior knowledge

--- e069.hs
+++ e069.hs
@@ -1,25 +1,8 @@
 import Data.Numbers.Primes
 import Data.List
-
-
-isEmpty :: [a] -> Bool
-isEmpty [] = True
-isEmpty _ = False
-
-sieve :: [Integer]
-sieve = 2:(sieveFilter 2 [3..])
-    where sieveFilter p (nextP:list) = nextP:(sieveFilter nextP (filter (\x -> x `mod` p /= 0) list))
-
-noOverlap :: (Eq a) => [a] -> [a] -> Bool
-noOverlap xs ys = isEmpty $ intersect xs ys
-
-factorList = [primeFactors a | a <- [1..1000000]]
-
-findCoprimeRatio :: (Fractional a) => Int -> a
-findCoprimeRatio n = (fromIntegral n)/(fromIntegral (length $ filter (noOverlap (factorList !! (n-1))) (take n factorList)))
-
-numFactors :: (Integral a) => a -> Int
-numFactors = length . nub . primeFactors
+    
+numCoprimes :: (Integral a) => a -> a
+numCoprimes n = round $ (*(fromIntegral n)) $ product $ map (\x -> (1-(1/(fromIntegral x))))$ nub $ primeFactors n
         
 main = do
-    print $ head $ filter ((== 7) . numFactors) [1..1000000]
+    print $ last $ sortOn (\x -> (fromIntegral x) / (fromIntegral (numCoprimes x))) [1..1000000]
