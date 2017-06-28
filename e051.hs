9a20135257ad79aa63d8c63785a72d7a3656bc17
kevin_wackman <kevin.wackman@gmail.com>
2017-06-28 17:16:20 -0500

    Remove old code i accidentally left in

--- e051.hs
+++ e051.hs
@@ -1,4 +1,3 @@
---UNFINISHED--
 import Data.Char
 import Data.List
 import Data.Numbers.Primes
@@ -43,65 +42,3 @@ checkFamily n =
     in (odd . head) enum && ((== 8) $ length $ filter isPrime $ enum)
 
 main = print $ concat $ map (filter checkFamily . nums 6) [3]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---hasThreeNL :: Int -> Bool
---hasThreeNL = (== 3) . last . sort . map length . group . sort . init . digits
---
---main = do
---    print $ length $ filter hasThreeNL $ dropWhile (< 1000) $ takeWhile (< 1000000) primes
