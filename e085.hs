dce152b25a88c736f4220acbacae1a13b18e6d6b
kevin_wackman <kevin.wackman@gmail.com>
2017-06-29 13:41:52 -0500

    Changed e085 so it prints out the answer instead of a tuple

--- e085.hs
+++ e085.hs
@@ -9,4 +9,4 @@ numRectangles :: Area -> Int
 numRectangles (x,y) = sum $ map (numSpecificRectangles (x,y)) [(x',y') | x'<-[1..x], y'<-[1..y]]
 
 main = do
-    print $ head $ sortBy (\(x,y) (x',y') -> y `compare` y') $ map (\x -> (x,(abs (2000000-(numRectangles x))))) [(x,y) | x<-[53..80], y<-[1..x]]
+    print $ (\((x,y),_) -> x*y) $ head $ sortBy (\(x,y) (x',y') -> y `compare` y') $ map (\x -> (x,(abs (2000000-(numRectangles x))))) [(x,y) | x<-[53..80], y<-[1..x]]
