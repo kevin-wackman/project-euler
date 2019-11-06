055d5fb52e7bed2ba7525db53fe5b8023c4ef31d
Kevin Wackman <kevin.wackman@gmail.com>
2019-11-06 13:31:14 -0600

    Refactor e102.hs
    I was looking through this file, and saw that it could be made more clear with explicitly spelling out what points are and what the axes represent.

--- e102.hs
+++ e102.hs
@@ -1,9 +1,10 @@
 import Data.List
 import Data.List.Split
 
-type Axis = Int
+data Axis = PositiveX | PositiveY | NegativeX | NegativeY deriving (Eq)
+type Point = (Int,Int)
 
-hasOrigin :: [(Int, Int)] -> Bool
+hasOrigin :: [Point] -> Bool
 hasOrigin [x,y,z] = (== 4) $ length $ nub $ (++ (getAxes x y)) $ (++ (getAxes x z)) $ getAxes y z
 
 isNegative :: (Num a, Ord a) => a -> Bool
@@ -24,28 +25,28 @@ areOpposing x y
     | isNegative x = isPositive y
     | otherwise    = isNegative y
     
-intercept :: Fractional a => (Int, Int) -> (Int, Int) -> a
+intercept :: Fractional a => Point -> Point -> a
 intercept (x1,y1) (x2,y2) = 
     let slope = (y2-y1) `div'` (x2-x1)
     in (fromIntegral y1) - (slope * (fromIntegral x1))
     
-checkX :: (Int, Int) -> (Int, Int) -> [Axis]
+checkX :: Point -> Point -> [Axis]
 checkX (x1,y1) (x2,y2)
     | not $ areOpposing x1 x2 = []
-    | otherwise = if isPositive $ intercept (x1,y1) (x2,y2) then [1] else [3]
+    | otherwise = if isPositive $ intercept (x1,y1) (x2,y2) then [PositiveX] else [NegativeX]
 
-checkY :: (Int, Int) -> (Int, Int) -> [Axis]
+checkY :: Point -> Point -> [Axis]
 checkY (x1,y1) (x2,y2)
     | not $ areOpposing y1 y2 = []
-    | otherwise = if isPositive $ intercept (y1,x1) (y2,x2) then [2] else [4]
+    | otherwise = if isPositive $ intercept (y1,x1) (y2,x2) then [PositiveY] else [NegativeY]
 
-getAxes :: (Int, Int) -> (Int, Int) -> [Axis]
+getAxes :: Point -> Point -> [Axis]
 getAxes p1 p2 = (checkX p1 p2) ++ (checkY p1 p2)
 
 lineToInts :: String -> [Int]
 lineToInts = map read . splitOn ","
 
-groupPoints :: [Int] -> [(Int, Int)]
+groupPoints :: [Int] -> [Point]
 groupPoints [] = []
 groupPoints (x:y:xs) = (x,y):(groupPoints xs)
 
@@ -53,4 +54,3 @@ groupPoints (x:y:xs) = (x,y):(groupPoints xs)
 main = do
     str <- readFile "e102in.txt"
     print $ length $ filter hasOrigin $ map (groupPoints . lineToInts) $ lines str
-        
